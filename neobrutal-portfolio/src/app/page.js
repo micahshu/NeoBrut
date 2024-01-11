3aa1a1d990d87e557fbc37a6665aac5fcfa845fc
Micah Shumaker <micah.shumaker@gmail.com>
2024-01-10 20:07:12 -0700

    commit

---
+++ neobrutal-portfolio/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='flex justify-center items-center  h-screen'>
+      <div className='w-40 h-40 bg-purple-600 brutShadow card'></div>
+    </div>
+  )
+}
